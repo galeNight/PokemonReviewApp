7aa619680edfe35f2a5013188857d475870d068f
galeNight <jannickmorup@gmail.com>
2024-01-24 13:28:19 +0100

    Add project files.

---
+++ PokemonReviewApp/Models/PokemonCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class PokemonCategory
+    {
+        public int PokemonId { get; set; }
+        public int CateroeyId { get; set; }
+        public Pokemon Pokemon { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
