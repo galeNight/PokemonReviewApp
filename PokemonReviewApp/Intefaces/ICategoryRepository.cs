e97be76f1a68eab4874ca34b04212a7fbfb5ffe8
galeNight <jannickmorup@gmail.com>
2024-02-01 09:47:50 +0100

    added code to interface

--- PokemonReviewApp/Intefaces/ICategoryRepository.cs
+++ PokemonReviewApp/Intefaces/ICategoryRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace PokemonReviewApp.Intefaces
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Intefaces
 {
     public interface ICategoryRepository
     {
+        ICollection<Category> GetCategories();
+        Category GetCategory(int id);
+        ICollection<Pokemon> GetPokemonByCategory(int categoryId);
+        bool CategoriesExists(int id);
     }
 }
