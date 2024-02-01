1846bfb8e60073a3be1ffd95c642fa8bf4840003
galeNight <jannickmorup@gmail.com>
2024-02-01 12:34:58 +0100

    added code to countrycontroller and program.cs

---
+++ PokemonReviewApp/Controllers/CrountryController.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Dto;
+using PokemonReviewApp.Intefaces;
+using PokemonReviewApp.Models;
+using PokemonReviewApp.Repository;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CrountryController :Controller
+    {
+        private readonly ICountryRepository _countryRepository;
+        private readonly IMapper _mapper;
+
+        public CrountryController(ICountryRepository countryRepository,IMapper mapper)
+        {
+            _countryRepository = countryRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+        public IActionResult GetPokemons()
+        {
+            var country = _mapper.Map<List<CountryDto>>(_countryRepository.GetCountries());
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(country);
+        }
+
+        [HttpGet("{countryId}")]
+        [ProducesResponseType(200, Type = typeof(Country))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemon(int countryId)
+        {
+            if (!_countryRepository.CountryExists(countryId))
+                return NotFound();
+
+            var country = _mapper.Map<CountryDto>(_countryRepository.GetCountry(countryId));
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(country);
+        }
+
+        [HttpGet("/owner/{ownerId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(200, Type = typeof(Country))]
+        public IActionResult getCountryOfAnOwner(int ownerId)
+        {
+            var county = _mapper.Map<CountryDto>(_countryRepository.GetCountryByOwner(ownerId));
+
+            if(!ModelState.IsValid)
+                return BadRequest();
+
+            return Ok(county);
+        }
+    }
+}
