c449f7f9206b2768793d4dbfdc391331170a0fc3
galeNight <jannickmorup@gmail.com>
2024-02-05 08:57:02 +0100

    added delete funtion

--- PokemonReviewApp/Controllers/CrountryController.cs
+++ PokemonReviewApp/Controllers/CrountryController.cs
@@ -114,5 +114,28 @@ namespace PokemonReviewApp.Controllers
             }
             return NoContent();
         }
+
+        [HttpDelete("{countryId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public IActionResult DeleteCategory(int countryId)
+        {
+            if (!_countryRepository.CountryExists(countryId))
+            {
+                return NotFound();
+            }
+
+            var countryToDelete = _countryRepository.GetCountry(countryId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (!_countryRepository.DeleteCountry(countryToDelete))
+            {
+                ModelState.AddModelError("", "Somthing went wrng deleting country");
+            }
+            return NoContent();
+        }
     }
 }
